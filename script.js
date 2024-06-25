fff5b5e72570fd1241121baff7abc3886046a741
Bruslli Lutaj <lutajbrus@gmail.com>
2024-06-25 20:06:11 +0200

    added another eventlistener

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ audio1.src = 'awesomeness.wav';
 button1.addEventListener('click', function()
 {
     audio1.play();
+    audio1.addEventListener('playing', function()
+    {
+        console.log('Audio 1 started playing!');
+    });
+    audio1.addEventListener('ended', function()
+    {
+
+    })
 })
