2bb5287ba8c5e53409b5cc7d1fdc9502153e7cca
Bruslli Lutaj <lutajbrus@gmail.com>
2024-06-27 16:09:13 +0200

    finished

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// When button1 gets clicked the file '.wav' gets reproduced
+
 const button1 = document.getElementById('button1');
 let audio1 = new Audio();
 audio1.src = 'awesomeness.wav';
+const audioCtx = new AudioContext();
+console.log(audioCtx);
 
 button1.addEventListener('click', function()
 {
@@ -13,4 +17,19 @@ button1.addEventListener('click', function()
     {
         console.log('Audio 1 ended!');
     });
-})
+});
+// When button2 gets clicked a little sound gets reproduced
+
+const button2 = document.getElementById('button2');
+button2.addEventListener('click', playSound);
+function playSound()
+{
+    const oscillator = audioCtx.createOscillator();
+    oscillator.connect(audioCtx.destination);
+    oscillator.type = 'triangle';
+    oscillator.start();
+    setTimeout(function()
+    {
+        oscillator.stop();
+    }, 100);
+}
